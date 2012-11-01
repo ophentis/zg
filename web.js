b03f4e5c4ef8b8861c9656aa7e8a8a6ed8e71e74
unknown <OOspirit@Ophentis.(none)>
2012-11-02 01:59:55 +0800

    Initial Commit

---
+++ web.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var app = express();
+
+//setup configuration
+
+
+app.configure('develop',function() {
+    app.use(express.logger());
+});
+
+app.configure(function() {
+    app.set('title', '式姬草子小幫手');
+
+    app.use(express.static(__dirname + '/css'));
+    app.use(express.static(__dirname + '/js'));
+    app.use(express.static(__dirname + '/lib'));
+    app.use(express.static(__dirname + '/partial'));
+});
+
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+//setup pages
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+});
+
+//init server and listen on port
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+}); 
