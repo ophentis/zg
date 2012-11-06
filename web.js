16483dc122c29d87f3af2dd97a6704ac4b8d465b
unknown <OOspirit@Ophentis.(none)>
2012-11-06 23:59:27 +0800

    added view folder and ejs template

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 //setup configuration
+app.set('view engine', 'ejs');
 
 
 app.configure('develop',function() {
@@ -12,7 +13,7 @@ app.configure('develop',function() {
 app.configure(function() {
     app.set('title', '式姬草子小幫手');
 
-	app.use('/', express.static(__dirname + '/html'));
+	// app.use('/', express.static(__dirname + '/html'));
     app.use('/css', express.static(__dirname + '/css'));
     app.use('/js', express.static(__dirname + '/js'));
     app.use('/lib', express.static(__dirname + '/lib'));
@@ -20,9 +21,9 @@ app.configure(function() {
 });
 
 //setup pages
-// app.get('/', function(request, response) {
-    // response.send('Hello World!');
-// });
+app.get('/', function(request, response) {
+    response.render('./view/index.ejs');
+});
 
 //init server and listen on port
 var port = process.env.PORT || 5000;
